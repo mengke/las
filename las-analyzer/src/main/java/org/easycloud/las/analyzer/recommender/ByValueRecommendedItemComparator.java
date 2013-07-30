7db403ca5d9cb2cc31b346234804223b1460b5e1
mengke <mengke@me.com>
2013-07-30 10:52:27 +0800

    添加Item相似度推荐

--- las-analyzer/src/main/java/org/easycloud/las/analyzer/recommender/ByValueRecommendedItemComparator.java
+++ las-analyzer/src/main/java/org/easycloud/las/analyzer/recommender/ByValueRecommendedItemComparator.java
@@ -1,10 +1,26 @@
 package org.easycloud.las.analyzer.recommender;
 
+import java.io.Serializable;
+import java.util.Comparator;
+
 /**
  * Created with IntelliJ IDEA.
  * User: ibntab
  * Date: 13-7-30
  * Time: 上午10:30
  */
-public class ByValueRecommendedItemComparator {
+public class ByValueRecommendedItemComparator implements Comparator<RecommendedItem>, Serializable {
+
+    private static final Comparator<RecommendedItem> INSTANCE = new ByValueRecommendedItemComparator();
+
+    public static Comparator<RecommendedItem> getInstance() {
+        return INSTANCE;
+    }
+
+    @Override
+    public int compare(RecommendedItem o1, RecommendedItem o2) {
+        double value1 = o1.getPrefValue();
+        double value2 = o2.getPrefValue();
+        return value1 > value2 ? -1 : value1 < value2 ? 1 : 0;
+    }
 }
